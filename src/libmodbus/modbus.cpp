5efdc6947ec7c22cb0d273a2066c347ace5b62ea
Your Name <you@example.com>
2018-11-23 17:14:42 +0800

    。

--- src/libmodbus/modbus.cpp
+++ src/libmodbus/modbus.cpp
@@ -481,7 +481,7 @@ int _modbus_receive_msg(modbus_t *ctx, uint8_t *msg, msg_type_t msg_type)
             for (i=0; i < rc; i++)
                 printf("<%.2X>", msg[msg_length + i]);
         }
-
+        if(msg[0]!=ctx->slave)continue;
         /* Sums bytes received */
         msg_length += rc;
         /* Computes remaining bytes */
